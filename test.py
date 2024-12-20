a1221b3f45398805d78ab6dc56a5ca4785903536
myphone12 <3396645909@qq.com>
2024-12-20 16:08:41 +0800

    modified:   test.py

--- test.py
+++ test.py
@@ -3,11 +3,13 @@ import time,functools
 def TestFun(fun):
     @functools.wraps(fun)
     def fun2(self, *args, **kwargs):
+        a = [];b = a
         self.innerwindow.showtext('['+ time.strftime("%H:%M:%S", time.localtime())+']' + ' -INFO- ' + 'Invoke function ' + fun.__name__ + '().')
         try:
             a = fun(self,*args, **kwargs)
         except Exception as e:
             self.innerwindow.showtext('['+ time.strftime("%H:%M:%S", time.localtime())+']' + ' -ERROR- ' + 'Error at function ' + fun.__name__ + '() :'+repr(e) + '.')
-        self.innerwindow.showtext('['+ time.strftime("%H:%M:%S", time.localtime())+']' + ' -INFO- Function ' + fun.__name__ + '() Return: '+ str(a) + '.')
+        if a is not b:
+            self.innerwindow.showtext('['+ time.strftime("%H:%M:%S", time.localtime())+']' + ' -INFO- Function ' + fun.__name__ + '() Return: '+ str(a) + '.')
         self.innerwindow.showtext('['+ time.strftime("%H:%M:%S", time.localtime())+']' + ' -INFO- ' + 'Finish function ' + fun.__name__ + '().')
     return fun2
