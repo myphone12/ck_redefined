8d05309c43868547e40e9386ae0b5aab3a0eb01a
myphone12 <3396645909@qq.com>
2024-12-22 23:53:16 +0800

    new file:   5.wav 	new file:   end.wav 	new file:   er.wav 	new file:   qi.wav 	modified:   w2a2.py 	new file:   "\345\225\212.wav" 	new file:   "\351\271\205.wav" 	new file:   "\351\271\2052.wav"

--- w2a2.py
+++ w2a2.py
@@ -1,4 +1,5 @@
-import wave,winsound,librosa
+import wave,pydub,ctypes,threading
+from pydub.playback import play
 import tkinter as tk
 from tkinter import ttk , messagebox as msg
 from UI import _UI
@@ -8,29 +9,46 @@ class Sound(_UI):
 
     def __init__(self):
         super().__init__()
+        self.textvar = tk.StringVar()
+        ctypes.windll.shell32.IsUserAnAdmin()
+        self.audio = [pydub.AudioSegment.from_wav(i) for i in ['5.wav','鹅.wav','啊.wav','鹅2.wav','er.wav']]
+        self.sound = [0,0,0,0,0]
+    
+    def Playsound(self,data):
+        self.thread = threading.Thread(target=lambda:self._Playsound(data))
+        self.thread.start()
+    
+    def _Playsound(self,data):
+        play(self.audio[data])
+        self.textvar.set('(' + ['5','鹅','啊','鹅','er'][data] + ', ' + str(round(self.items['Entry'][data*3].get(),2)) + ', ' + str(round(self.items['Entry'][data*3+1].get(),2)) + ', ' + str(round(self.items['Entry'][data*3+2].get(),2)) + ')')
 
+    def a(self,event,num):
+        print(num,event)
+        if num[1] == 0:
+            self.audio[num[0]] = self.audio[num[0]] + (float(event) - self.sound[num[0]])
+            self.sound[num[0]] = float(event)
+    
     def InputLoading(self):
-        for i in range(4):
+        for i in range(5):
             for j in range(3):
-                self.items['Entry'].append(ttk.Scale(self.items['Text'][i],from_=100,to=0,orient='vertical'))
+                self.items['Entry'].append(ttk.Scale(self.items['Text'][i],from_=10,to=-10,orient='vertical',command=lambda tmp,data=(i,j):self.a(tmp,data)))
                 self.items['Entry'][-1].grid(row=0,column=j,padx=10,pady=10)
+                self.items['Text'].append(ttk.Label(self.items['Text'][i],text=['音量','音调','速度'][j]))
+                self.items['Text'][-1].grid(row=1,column=j,padx=10,pady=10)
     
     def ButtonLoading(self):
-        self.button1 = ttk.Button(self.tk, text='5',width=20)
-        self.button1.grid(row=2,column=0,padx=20,pady=20)
-        self.button2 = ttk.Button(self.tk, text='鹅',width=20)
-        self.button2.grid(row=2,column=1,padx=20,pady=20)
-        self.button3 = ttk.Button(self.tk, text='啊',width=20)
-        self.button3.grid(row=2,column=2,padx=20,pady=20)
-        self.button4 = ttk.Button(self.tk,text='鹅',width=20)
-        self.button4.grid(row=2,column=3,padx=20,pady=20)
+        for i in range(5):
+            self.items['Button'].append(ttk.Button(self.tk, text=['5','鹅','啊','鹅','er'][i],width=20,command=lambda data=i:self.Playsound(data)))
+            self.items['Button'][-1].grid(row=2,column=i,padx=20,pady=20)
         
-        self.pp()
+        # self.pp()
     
     def TextLoading(self):
-        for i in range(4):
+        for i in range(5):
             self.items['Text'].append(ttk.Frame(self.tk,relief='sunken',borderwidth=1))
             self.items['Text'][-1].grid(row=1,column=i)
+        self.items['Text'].append(ttk.Label(self.tk,textvariable=self.textvar))
+        self.items['Text'][-1].grid(row=3,column=0,columnspan=5,padx=10,pady=10)
     
     def pp(self):
         print(self.items['Entry'][0].get())
